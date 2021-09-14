77dbbe93cc3143f2dd330df1626f0eff13b65f8b
Edward Sanchez <86178264+EdSanchez97@users.noreply.github.com>
2021-09-14 01:45:04 -0500

    version1
    the firts verison of the portfolio!

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+async function init () {
+    const node = document.querySelector("#type-text")
+    
+    await sleep(1000)
+    node.innerText = ""
+    await node.type('Im ')
+    
+    while (true) {
+      await node.type('a Front-end Developer!')
+      await sleep(2000)
+      await node.delete('a Front-end Developer!')
+      await node.type('a TeamPlayer!')
+      await sleep(2000)
+      await node.delete('a TeamPlayer!')
+      await node.type('Hard worker!')
+      await sleep(2000)
+      await node.delete('Hard worker!')
+    }
+  }
+
+  
+  const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+  
+  class TypeAsync extends HTMLSpanElement {
+    get typeInterval () {
+      const randomMs = 100 * Math.random()
+      return randomMs < 50 ? 10 : randomMs
+    }
+    
+    async type (text) {
+      for (let character of text) {
+        this.innerText += character
+        await sleep(this.typeInterval)
+      }
+    }
+    
+    async delete (text) {
+      for (let character of text) {
+        this.innerText = this.innerText.slice(0, this.innerText.length -1)
+        await sleep(this.typeInterval)
+      }
+    }
+  }
+  
+  customElements.define('type-async', TypeAsync, { extends: 'span' })
+  
+  
+  init()
+
+
+const scrollOffset = 100;
+ 
+const scrollElement = document.querySelector(".js-scroll");
+ 
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+ 
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+  );
+};
+ 
+const displayScrollElement = () => {
+  scrollElement.classList.add('scrolled');
+}
+ 
+const hideScrollElement = () => {
+  scrollElement.classList.remove('scrolled');
+}
+ 
+const handleScrollAnimation = () => {
+  if (elementInView(scrollElement, scrollOffset)) {
+      displayScrollElement();
+  } else {
+    hideScrollElement();
+  }
+}
+ 
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+})
